bd3c694fea940b2c0ea1b539b227b352b1fa7db2
MicHaeLNgSd <misha.grigorov2020@gmail.com>
2022-10-08 20:13:25 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
 
-cartButton.addEventListener('click',toggleModal);
-close.addEventListener('click',toggleModal);
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
 
-function toggleModal(){
-    modal.classList.toggle("is-open");
-}
-
-new WOW().init();
-=======
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
-
-cartButton.addEventListener('click',toggleModal);
-close.addEventListener('click',toggleModal);
-
-function toggleModal(){
+function toggleModal() {
     modal.classList.toggle("is-open");
 }
 
@@ -27,60 +13,58 @@ new WOW().init();
 
 ///day1
 
-const buttonAuth = document.querySelector('.button-auth');
-const modalAuth = document.querySelector('.modal-auth');
-const closeAuth = document.querySelector('.close-auth');
-const logInForm = document.querySelector('#logInForm');
-const loginInput = document.querySelector('#login');
-const passwordInput = document.querySelector('#password');
-const userName = document.querySelector('.user-name');
-const buttonOut = document.querySelector('.button-out');
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const passwordInput = document.querySelector("#password");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
 
-let login = localStorage.getItem('gloDelivery');
+let login = localStorage.getItem("gloDelivery");
 let k = 0;
 function toogleModalAuth() {
-    modalAuth.classList.toggle("is-open")
+    modalAuth.classList.toggle("is-open");
 }
 
-
-function autorized () {
-    function logOut(){
+function autorized() {
+    function logOut() {
         login = null;
-        localStorage.removeItem('gloDelivery');
+        localStorage.removeItem("gloDelivery");
         userName.textContent = login;
 
-        buttonAuth.style.display = '';
-        userName.style.display = '';
-        buttonOut.style.display = '';
+        buttonAuth.style.display = "";
+        userName.style.display = "";
+        buttonOut.style.display = "";
 
-        buttonOut.removeEventListener('click', logOut);
+        buttonOut.removeEventListener("click", logOut);
 
         checkAuth();
     }
 
     userName.textContent = login;
-    cartButton.style.display = '';
+    cartButton.style.display = "";
     buttonAuth.style.display = "none";
     userName.style.display = "block";
     buttonOut.style.display = "block";
 
-    buttonOut.addEventListener('click', logOut);
+    buttonOut.addEventListener("click", logOut);
 }
-function notAutorized () {
-    function logIn (event) {
-        if(loginInput.value && passwordInput.value)
-        {
+function notAutorized() {
+    function logIn(event) {
+        if (loginInput.value && passwordInput.value) {
             event.preventDefault();
             login = loginInput.value;
 
-            localStorage.setItem('gloDelivery', login);
+            localStorage.setItem("gloDelivery", login);
             toogleModalAuth();
-            buttonAuth.removeEventListener('click', toogleModalAuth);
-            closeAuth.removeEventListener('click', toogleModalAuth);
-            logInForm.removeEventListener('submit', logIn);
+            buttonAuth.removeEventListener("click", toogleModalAuth);
+            closeAuth.removeEventListener("click", toogleModalAuth);
+            logInForm.removeEventListener("submit", logIn);
             logInForm.reset();
             checkAuth();
-        } else{
+        } else {
             alert("Введіть логін та пароль");
             event.preventDefault();
             toogleModalAuth();
@@ -89,16 +73,15 @@ function notAutorized () {
     buttonOut.style.display = "none";
     cartButton.style.display = "none";
 
-    buttonAuth.addEventListener('click', toogleModalAuth);
-    closeAuth.addEventListener('click', toogleModalAuth);
-    logInForm.addEventListener('submit', logIn);
+    buttonAuth.addEventListener("click", toogleModalAuth);
+    closeAuth.addEventListener("click", toogleModalAuth);
+    logInForm.addEventListener("submit", logIn);
 }
 function checkAuth() {
-    if(login){
+    if (login) {
         autorized();
     } else {
         notAutorized();
     }
 }
 checkAuth();
->>>>>>> eaeee25 (first commit)
